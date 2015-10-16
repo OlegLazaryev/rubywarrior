8692bd675b9e4e25e04356477d6865d6069af5c0
OlegLazaryev <olegl@interlink.ua.com>
2015-10-16 17:06:24 +0300

    Improve naming in Environment.

--- environment.rb
+++ environment.rb
@@ -1,50 +1,48 @@
 class Environment
 
-	def initialize(warrior)
-		@warrior = warrior
-	end
+  def initialize(warrior)
+    @warrior = warrior
+  end
 
-	def archer_backward?
-  		space  = detect_closest_unit(:backward)
-  		return unless space  		
-  		unit = space.unit
-  		return unless unit
-  		unit.name == 'Archer'
-    end
+  def archer_backward?
+		space = detect_closest_space(:backward)
+		unit = space && space.unit
+		unit && unit.kind_of?(RubyWarrior::Units::Archer)
+  end
 
-    def enemy_forward?
-		closest_space = detect_closest_unit
-		closest_space && closest_space.enemy?
-    end
+  def enemy_forward?
+	  closest_space = detect_closest_space
+	  closest_space && closest_space.enemy?
+  end
 
-    def wall_forward?
-    	warrior.feel.wall?
-    end
+  def wall_forward?
+  	warrior.feel.wall?
+  end
 
-    def nothing_forward?
-    	warrior.feel.empty?
-    end
+  def nothing_forward?
+  	warrior.feel.empty?
+  end
 
-    def safe_attack?
-    	no_captives_detected? && enemy_detected?
-    end
+  def safe_attack?
+  	no_captives_detected? && enemy_detected?
+  end
 
-    private
+  private
 
-    attr_reader :warrior
+  attr_reader :warrior
 
-    def detect_closest_unit(direction = :forward)
-  		look = warrior.look(direction)
-    	look.detect { |space| !space.empty? }
-    end
+  def detect_closest_space(direction = :forward)
+		look = warrior.look(direction)
+  	look.detect { |space| !space.empty? }
+  end
 
-    def no_captives_detected?
-    	!warrior.look.any?(&:captive?)
-    end
+  def no_captives_detected?
+  	!warrior.look.any?(&:captive?)
+  end
 
-    def enemy_detected?
-    	warrior.look.any?(&:enemy?)
-    end
+  def enemy_detected?
+  	warrior.look.any?(&:enemy?)
+  end
 
 
 end
