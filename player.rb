15a9cfd2e8951547b731ba50ab8cb843ef81011b
OlegLazaryev <olegl@interlink.ua.com>
2015-10-16 16:26:23 +0300

    Ninth level

--- player.rb
+++ player.rb
@@ -1,18 +1,21 @@
 class Player
+
 	def initialize
 		@health = 0
 	end
 
   def play_turn(warrior)
-  	warrior.pivot! and return if warrior.feel.wall?
-
-  	
+  	warrior.pivot! and return if warrior.feel.wall? || archer_backward?(warrior)  	
 
     if warrior.feel.empty?
     	if (warrior.health < 20)
-    		if !damage?(warrior)
+    		look = warrior.look
+    		closest_space = detect_closest_unit(warrior)
+    		if closest_space && closest_space.enemy?
+    			warrior.shoot!
+    		elsif !damage?(warrior)
     			warrior.rest!
-			elsif warrior.health < 6
+			elsif warrior.health < 12
 				warrior.walk!(:backward)
 			else
 				warrior.walk!
@@ -35,6 +38,20 @@ class Player
   end
 
   private
+
+  def archer_backward?(warrior)
+  	space  = detect_closest_unit(warrior, :backward)
+  	return unless space  		
+  	unit = space.unit
+  	puts 'archer_backward?'
+  	puts unit.name
+  	unit.name == 'Archer'
+  end
+
+  def detect_closest_unit(warrior, direction = :forward)
+  	look = warrior.look(direction)
+    look.detect { |space| !space.empty? }
+  end
 
   def damage?(warrior)
   	@health > warrior.health
